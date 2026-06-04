6871039e25b6b3391cef05df6e9455c03ceb11b3
esmail1000 <esmailasaad668@gmail.com>
2026-06-04 23:09:37 +0300

    fix nlp inference padding boolean

--- nlp_engine/inference.py
+++ nlp_engine/inference.py
@@ -759,7 +759,7 @@ def _sliding_windows(
         stride=stride,
         return_overflowing_tokens=True,
         return_offsets_mapping=True,
-        padding=true,
+        padding=True,
         return_tensors="pt",
     )
 
